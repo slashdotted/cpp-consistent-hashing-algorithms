9e87031f02174b355c2a32557c6adbd85028e522
Amos Brocco <contact@amosbrocco.ch>
2023-12-06 10:30:12 +0100

    Move implementation of methods inside class to improve readability

--- memento/mementoengine.h
+++ memento/mementoengine.h
@@ -27,7 +27,10 @@ public:
    *
    * @param size          initial number of working buckets (0 < size)
    */
-  MementoEngine(uint32_t, uint32_t size);
+    MementoEngine(uint32_t, uint32_t size)
+        : m_lastRemoved{size}
+        , m_bArraySize{size}
+    {}
 
   /**
    * Returns the bucket where the given key should be mapped.
@@ -35,7 +38,49 @@ public:
    * @param key the key to map
    * @return the related bucket
    */
-  uint32_t getBucket(std::string_view key) const noexcept;
+    uint32_t getBucket(std::string_view key) const noexcept {
+        /*
+   * We invoke JumpHash to get a bucket
+   * in the range [0,bArraySize-1].
+   */
+        const auto hash{XXH64(key.data(), key.size(), 0)};
+        auto b = JumpConsistentHash(hash, m_bArraySize);
+
+        /*
+   * We check if the bucket was removed, if not we are done.
+   * If the bucket was removed the replacing bucket is >= 0,
+   * otherwise it is -1.
+   */
+        auto replacer = m_memento.replacer(b);
+        while (replacer >= 0) {
+
+            /*
+     * If the bucket was removed, we must re-hash and find
+     * a new bucket in the remaining slots. To know the
+     * remaining slots, we look at 'replacer' that also
+     * represents the size of the working set when the bucket
+     * was removed and get a new bucket in [0,replacer-1].
+     */
+            const auto h = XXH64(key.data(), key.size(), b);
+            b = h % replacer;
+
+            /*
+     * If we hit a removed bucket we follow the replacements
+     * until we get a working bucket or a bucket in the range
+     * [0,replacer-1]
+     */
+            auto r = m_memento.replacer(b);
+            while (r >= replacer) {
+                b = r;
+                r = m_memento.replacer(b);
+            }
+
+            /* Finally we update the entry of the external loop. */
+            replacer = r;
+        }
+
+        return b;
+    }
 
   /**
    * Returns the bucket where the given key should be mapped.
@@ -45,14 +90,73 @@ public:
    * @param key the initial seed for CRC32c
    * @return the related bucket
    */
-  uint32_t getBucketCRC32c(uint64_t key, uint64_t seed) const noexcept;
+  uint32_t getBucketCRC32c(uint64_t key, uint64_t seed) const noexcept {
+      const auto hash = crc32c_sse42_u64(key, seed);
+      /*
+   * We invoke JumpHash to get a bucket
+   * in the range [0,bArraySize-1].
+   */
+      auto b = JumpConsistentHash(hash, m_bArraySize);
+
+      /*
+   * We check if the bucket was removed, if not we are done.
+   * If the bucket was removed the replacing bucket is >= 0,
+   * otherwise it is -1.
+   */
+      auto replacer = m_memento.replacer(b);
+      while (replacer >= 0) {
+
+          /*
+     * If the bucket was removed, we must re-hash and find
+     * a new bucket in the remaining slots. To know the
+     * remaining slots, we look at 'replacer' that also
+     * represents the size of the working set when the bucket
+     * was removed and get a new bucket in [0,replacer-1].
+     */
+          const auto h = crc32c_sse42_u64(key, b);
+          b = h % replacer;
+
+          /*
+     * If we hit a removed bucket we follow the replacements
+     * until we get a working bucket or a bucket in the range
+     * [0,replacer-1]
+     */
+          auto r = m_memento.replacer(b);
+          while (r >= replacer) {
+              b = r;
+              r = m_memento.replacer(b);
+          }
+
+          /* Finally we update the entry of the external loop. */
+          replacer = r;
+      }
+
+      return b;
+  }
 
   /**
    * Adds a new bucket to the engine.
    *
    * @return the added bucket
    */
-  uint32_t addBucket() noexcept;
+  uint32_t addBucket() noexcept {
+      /* The new bucket to add is the last removed one. */
+      auto bucket = m_lastRemoved;
+
+      /**
+   * We restore the bucket from the replacement set
+   * and update the value of the last removed bucket.
+   */
+      m_lastRemoved = m_memento.restore(bucket);
+
+      /**
+   * If the restored bucket is 'bArraySize'
+   * we must update the actual size by 1.
+   */
+      m_bArraySize = m_bArraySize > bucket ? m_bArraySize : bucket + 1;
+
+      return bucket;
+  }
 
   /**
    * Removes the given bucket from the engine.
@@ -60,21 +164,41 @@ public:
    * @param bucket the bucket to remove
    * @return the removed bucket
    */
-  uint32_t removeBucket(uint32_t bucket) noexcept;
+  uint32_t removeBucket(uint32_t bucket) noexcept {
+      /*
+   * If the lookup table is empty and the bucket to remove is the last one,
+   * we are in the same use case as JumpHash. In this case we don't need
+   * to remember the bucket, we just need to reduce the size of the b-array.
+   */
+      if (m_memento.isEmpty() && bucket == m_bArraySize - 1) {
+          m_lastRemoved = m_bArraySize = bucket;
+          return bucket;
+      }
+
+      /* Otherwise, we add the entry to the memento table using the removed bucket
+   * as the key. */
+      m_lastRemoved = m_memento.remember(bucket, size() - 1, m_lastRemoved);
+
+      return bucket;
+  }
 
   /**
    * Returns the size of the working set.
    *
    * @return size of the working set.
    */
-  uint32_t size() const noexcept;
+  uint32_t size() const noexcept {
+      return m_bArraySize - m_memento.size();
+  }
 
   /**
    * Returns the size of the b-array.
    *
    * @return the size of the b-array.
    */
-  uint32_t bArraySize() const noexcept;
+  uint32_t bArraySize() const noexcept {
+      return m_bArraySize;
+  }
 
 private:
   // From AnchorHash
@@ -100,180 +224,5 @@ private:
   uint32_t m_bArraySize;
   uint32_t m_lastRemoved;
 };
-
-/**
- * Creates a new MementoHash engine.
- *
- * @param size          initial number of working buckets (0 < size)
- */
-template<template<typename...> class MementoMap, typename... Args>
-MementoEngine<MementoMap, Args...>::MementoEngine(uint32_t, uint32_t size)
-    : m_lastRemoved{size}
-    , m_bArraySize{size}
-{}
-
-template <template <typename...> class MementoMap, typename... Args>
-uint32_t MementoEngine<MementoMap, Args...>::getBucket(
-    std::string_view key) const noexcept {
-  /*
-   * We invoke JumpHash to get a bucket
-   * in the range [0,bArraySize-1].
-   */
-  const auto hash{XXH64(key.data(), key.size(), 0)};
-  auto b = JumpConsistentHash(hash, m_bArraySize);
-
-  /*
-   * We check if the bucket was removed, if not we are done.
-   * If the bucket was removed the replacing bucket is >= 0,
-   * otherwise it is -1.
-   */
-  auto replacer = m_memento.replacer(b);
-  while (replacer >= 0) {
-
-    /*
-     * If the bucket was removed, we must re-hash and find
-     * a new bucket in the remaining slots. To know the
-     * remaining slots, we look at 'replacer' that also
-     * represents the size of the working set when the bucket
-     * was removed and get a new bucket in [0,replacer-1].
-     */
-    const auto h = XXH64(key.data(), key.size(), b);
-    b = h % replacer;
-
-    /*
-     * If we hit a removed bucket we follow the replacements
-     * until we get a working bucket or a bucket in the range
-     * [0,replacer-1]
-     */
-    auto r = m_memento.replacer(b);
-    while (r >= replacer) {
-      b = r;
-      r = m_memento.replacer(b);
-    }
-
-    /* Finally we update the entry of the external loop. */
-    replacer = r;
-  }
-
-  return b;
-}
-
-template <template <typename...> class MementoMap, typename... Args>
-uint32_t MementoEngine<MementoMap, Args...>::getBucketCRC32c(
-    uint64_t key, uint64_t seed) const noexcept {
-  const auto hash = crc32c_sse42_u64(key, seed);
-  /*
-   * We invoke JumpHash to get a bucket
-   * in the range [0,bArraySize-1].
-   */
-  auto b = JumpConsistentHash(hash, m_bArraySize);
-
-  /*
-   * We check if the bucket was removed, if not we are done.
-   * If the bucket was removed the replacing bucket is >= 0,
-   * otherwise it is -1.
-   */
-  auto replacer = m_memento.replacer(b);
-  while (replacer >= 0) {
-
-    /*
-     * If the bucket was removed, we must re-hash and find
-     * a new bucket in the remaining slots. To know the
-     * remaining slots, we look at 'replacer' that also
-     * represents the size of the working set when the bucket
-     * was removed and get a new bucket in [0,replacer-1].
-     */
-    const auto h = crc32c_sse42_u64(key, b);
-    b = h % replacer;
-
-    /*
-     * If we hit a removed bucket we follow the replacements
-     * until we get a working bucket or a bucket in the range
-     * [0,replacer-1]
-     */
-    auto r = m_memento.replacer(b);
-    while (r >= replacer) {
-      b = r;
-        r = m_memento.replacer(b);
-    }
-
-    /* Finally we update the entry of the external loop. */
-    replacer = r;
-  }
-
-  return b;
-}
-
-/**
- * Adds a new bucket to the engine.
- *
- * @return the added bucket
- */
-template <template <typename...> class MementoMap, typename... Args>
-uint32_t MementoEngine<MementoMap, Args...>::addBucket() noexcept {
-  /* The new bucket to add is the last removed one. */
-  auto bucket = m_lastRemoved;
-
-  /**
-   * We restore the bucket from the replacement set
-   * and update the value of the last removed bucket.
-   */
-  m_lastRemoved = m_memento.restore(bucket);
-
-  /**
-   * If the restored bucket is 'bArraySize'
-   * we must update the actual size by 1.
-   */
-  m_bArraySize = m_bArraySize > bucket ? m_bArraySize : bucket + 1;
-
-  return bucket;
-}
-
-/**
- * Removes the given bucket from the engine.
- *
- * @param bucket the bucket to remove
- * @return the removed bucket
- */
-template <template <typename...> class MementoMap, typename... Args>
-uint32_t
-MementoEngine<MementoMap, Args...>::removeBucket(uint32_t bucket) noexcept {
-  /*
-   * If the lookup table is empty and the bucket to remove is the last one,
-   * we are in the same use case as JumpHash. In this case we don't need
-   * to remember the bucket, we just need to reduce the size of the b-array.
-   */
-  if (m_memento.isEmpty() && bucket == m_bArraySize - 1) {
-    m_lastRemoved = m_bArraySize = bucket;
-    return bucket;
-  }
-
-  /* Otherwise, we add the entry to the memento table using the removed bucket
-   * as the key. */
-  m_lastRemoved = m_memento.remember(bucket, size() - 1, m_lastRemoved);
-
-  return bucket;
-}
-
-/**
- * Returns the size of the working set.
- *
- * @return size of the working set.
- */
-template <template <typename...> class MementoMap, typename... Args>
-inline uint32_t MementoEngine<MementoMap, Args...>::size() const noexcept {
-  return m_bArraySize - m_memento.size();
-}
-
-/**
- * Returns the size of the b-array.
- *
- * @return the size of the b-array.
- */
-template <template <typename...> class MementoMap, typename... Args>
-inline uint32_t
-MementoEngine<MementoMap, Args...>::bArraySize() const noexcept {
-  return m_bArraySize;
-}
 
 #endif // MEMENTOENGINE_H
