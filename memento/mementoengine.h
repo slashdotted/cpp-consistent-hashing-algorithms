f8a2a7c0ba7a2750fc7a027f7f6d45edefafe713
Amos Brocco <amos.brocco@gmail.com>
2023-12-06 14:20:29 +0100

    Check if it is possible to use Jump at the beginning of getBucket

--- memento/mementoengine.h
+++ memento/mementoengine.h
@@ -27,10 +27,8 @@ public:
    *
    * @param size          initial number of working buckets (0 < size)
    */
-    MementoEngine(uint32_t, uint32_t size)
-        : m_lastRemoved{size}
-        , m_bArraySize{size}
-    {}
+  MementoEngine(uint32_t, uint32_t size)
+      : m_lastRemoved{size}, m_bArraySize{size} {}
 
   /**
    * Returns the bucket where the given key should be mapped.
@@ -38,50 +36,55 @@ public:
    * @param key the key to map
    * @return the related bucket
    */
-    uint32_t getBucket(std::string_view key) const noexcept {
-        /*
-   * We invoke JumpHash to get a bucket
-   * in the range [0,bArraySize-1].
-   */
-        const auto hash{XXH64(key.data(), key.size(), 0)};
-        auto b = JumpConsistentHash(hash, m_bArraySize);
+  uint32_t getBucket(std::string_view key) const noexcept {
+    const auto hash{XXH64(key.data(), key.size(), 0)};
 
-        /*
-   * We check if the bucket was removed, if not we are done.
-   * If the bucket was removed the replacing bucket is >= 0,
-   * otherwise it is -1.
-   */
-        if (m_lastRemoved == m_bArraySize) return b;
-        auto replacer = m_memento.replacer(b);
-        while (replacer >= 0) {
-
-            /*
-     * If the bucket was removed, we must re-hash and find
-     * a new bucket in the remaining slots. To know the
-     * remaining slots, we look at 'replacer' that also
-     * represents the size of the working set when the bucket
-     * was removed and get a new bucket in [0,replacer-1].
-     */
-            const auto h = XXH64(key.data(), key.size(), b);
-            b = h % replacer;
-
-            /*
-     * If we hit a removed bucket we follow the replacements
-     * until we get a working bucket or a bucket in the range
-     * [0,replacer-1]
-     */
-            auto r = m_memento.replacer(b);
-            while (r >= replacer) {
-                b = r;
-                r = m_memento.replacer(b);
-            }
-
-            /* Finally we update the entry of the external loop. */
-            replacer = r;
-        }
-
-        return b;
+    /* If there are no removed nodes, just perform Jump */
+    if (m_lastRemoved == m_bArraySize) {
+      return JumpConsistentHash(hash, m_bArraySize);
     }
+
+    /*
+     * We invoke JumpHash to get a bucket
+     * in the range [0,bArraySize-1].
+     */
+    auto b = JumpConsistentHash(hash, m_bArraySize);
+
+    /*
+     * We check if the bucket was removed, if not we are done.
+     * If the bucket was removed the replacing bucket is >= 0,
+     * otherwise it is -1.
+     */
+    auto replacer = m_memento.replacer(b);
+    while (replacer >= 0) {
+
+      /*
+       * If the bucket was removed, we must re-hash and find
+       * a new bucket in the remaining slots. To know the
+       * remaining slots, we look at 'replacer' that also
+       * represents the size of the working set when the bucket
+       * was removed and get a new bucket in [0,replacer-1].
+       */
+      const auto h = XXH64(key.data(), key.size(), b);
+      b = h % replacer;
+
+      /*
+       * If we hit a removed bucket we follow the replacements
+       * until we get a working bucket or a bucket in the range
+       * [0,replacer-1]
+       */
+      auto r = m_memento.replacer(b);
+      while (r >= replacer) {
+        b = r;
+        r = m_memento.replacer(b);
+      }
+
+      /* Finally we update the entry of the external loop. */
+      replacer = r;
+    }
+
+    return b;
+  }
 
   /**
    * Returns the bucket where the given key should be mapped.
@@ -92,48 +95,53 @@ public:
    * @return the related bucket
    */
   uint32_t getBucketCRC32c(uint64_t key, uint64_t seed) const noexcept {
-      const auto hash = crc32c_sse42_u64(key, seed);
-      /*
-   * We invoke JumpHash to get a bucket
-   * in the range [0,bArraySize-1].
-   */
-      auto b = JumpConsistentHash(hash, m_bArraySize);
+    const auto hash = crc32c_sse42_u64(key, seed);
 
-      /*
-   * We check if the bucket was removed, if not we are done.
-   * If the bucket was removed the replacing bucket is >= 0,
-   * otherwise it is -1.
-   */
-      if (m_lastRemoved == m_bArraySize) return b;
-      auto replacer = m_memento.replacer(b);
-      while (replacer >= 0) {
+    /* If there are no removed nodes, just perform Jump */
+    if (m_lastRemoved == m_bArraySize) {
+      return JumpConsistentHash(hash, m_bArraySize);
+    }
 
-          /*
-     * If the bucket was removed, we must re-hash and find
-     * a new bucket in the remaining slots. To know the
-     * remaining slots, we look at 'replacer' that also
-     * represents the size of the working set when the bucket
-     * was removed and get a new bucket in [0,replacer-1].
+    /*
+     * We invoke JumpHash to get a bucket
+     * in the range [0,bArraySize-1].
      */
-          const auto h = crc32c_sse42_u64(key, b);
-          b = h % replacer;
+    auto b = JumpConsistentHash(hash, m_bArraySize);
 
-          /*
-     * If we hit a removed bucket we follow the replacements
-     * until we get a working bucket or a bucket in the range
-     * [0,replacer-1]
+    /*
+     * We check if the bucket was removed, if not we are done.
+     * If the bucket was removed the replacing bucket is >= 0,
+     * otherwise it is -1.
      */
-          auto r = m_memento.replacer(b);
-          while (r >= replacer) {
-              b = r;
-              r = m_memento.replacer(b);
-          }
+    auto replacer = m_memento.replacer(b);
+    while (replacer >= 0) {
 
-          /* Finally we update the entry of the external loop. */
-          replacer = r;
+      /*
+       * If the bucket was removed, we must re-hash and find
+       * a new bucket in the remaining slots. To know the
+       * remaining slots, we look at 'replacer' that also
+       * represents the size of the working set when the bucket
+       * was removed and get a new bucket in [0,replacer-1].
+       */
+      const auto h = crc32c_sse42_u64(key, b);
+      b = h % replacer;
+
+      /*
+       * If we hit a removed bucket we follow the replacements
+       * until we get a working bucket or a bucket in the range
+       * [0,replacer-1]
+       */
+      auto r = m_memento.replacer(b);
+      while (r >= replacer) {
+        b = r;
+        r = m_memento.replacer(b);
       }
 
-      return b;
+      /* Finally we update the entry of the external loop. */
+      replacer = r;
+    }
+
+    return b;
   }
 
   /**
@@ -142,22 +150,22 @@ public:
    * @return the added bucket
    */
   uint32_t addBucket() noexcept {
-      /* The new bucket to add is the last removed one. */
-      auto bucket = m_lastRemoved;
+    /* The new bucket to add is the last removed one. */
+    auto bucket = m_lastRemoved;
 
-      /**
-   * We restore the bucket from the replacement set
-   * and update the value of the last removed bucket.
-   */
-      m_lastRemoved = m_memento.restore(bucket);
+    /**
+     * We restore the bucket from the replacement set
+     * and update the value of the last removed bucket.
+     */
+    m_lastRemoved = m_memento.restore(bucket);
 
-      /**
-   * If the restored bucket is 'bArraySize'
-   * we must update the actual size by 1.
-   */
-      m_bArraySize = m_bArraySize > bucket ? m_bArraySize : bucket + 1;
+    /**
+     * If the restored bucket is 'bArraySize'
+     * we must update the actual size by 1.
+     */
+    m_bArraySize = m_bArraySize > bucket ? m_bArraySize : bucket + 1;
 
-      return bucket;
+    return bucket;
   }
 
   /**
@@ -167,21 +175,21 @@ public:
    * @return the removed bucket
    */
   uint32_t removeBucket(uint32_t bucket) noexcept {
-      /*
-   * If the lookup table is empty and the bucket to remove is the last one,
-   * we are in the same use case as JumpHash. In this case we don't need
-   * to remember the bucket, we just need to reduce the size of the b-array.
-   */
-      if ((m_lastRemoved == m_bArraySize) && bucket == m_bArraySize - 1) {
-          m_lastRemoved = m_bArraySize = bucket;
-          return bucket;
-      }
-
-      /* Otherwise, we add the entry to the memento table using the removed bucket
-   * as the key. */
-      m_lastRemoved = m_memento.remember(bucket, size() - 1, m_lastRemoved);
-
+    /*
+     * If the lookup table is empty and the bucket to remove is the last one,
+     * we are in the same use case as JumpHash. In this case we don't need
+     * to remember the bucket, we just need to reduce the size of the b-array.
+     */
+    if ((m_lastRemoved == m_bArraySize) && bucket == m_bArraySize - 1) {
+      m_lastRemoved = m_bArraySize = bucket;
       return bucket;
+    }
+
+    /* Otherwise, we add the entry to the memento table using the removed bucket
+     * as the key. */
+    m_lastRemoved = m_memento.remember(bucket, size() - 1, m_lastRemoved);
+
+    return bucket;
   }
 
   /**
@@ -189,37 +197,33 @@ public:
    *
    * @return size of the working set.
    */
-  uint32_t size() const noexcept {
-      return m_bArraySize - m_memento.size();
-  }
+  uint32_t size() const noexcept { return m_bArraySize - m_memento.size(); }
 
   /**
    * Returns the size of the b-array.
    *
    * @return the size of the b-array.
    */
-  uint32_t bArraySize() const noexcept {
-      return m_bArraySize;
-  }
+  uint32_t bArraySize() const noexcept { return m_bArraySize; }
 
 private:
   // From AnchorHash
   static uint32_t crc32c_sse42_u64(uint64_t key, uint64_t seed) {
-      __asm__ volatile("crc32q %[key], %[seed];"
-                       : [seed] "+r"(seed)
-                       : [key] "rm"(key));
-      return seed;
+    __asm__ volatile("crc32q %[key], %[seed];"
+                     : [seed] "+r"(seed)
+                     : [key] "rm"(key));
+    return seed;
   }
 
   // From Jump paper
   static int32_t JumpConsistentHash(uint64_t key, int32_t num_buckets) {
-      int64_t b = 1, j = 0;
-      while (j < num_buckets) {
-          b = j;
-          key = key * 2862933555777941757ULL + 1;
-          j = (b + 1) * (double(1LL << 31) / double((key >> 33) + 1));
-      }
-      return b;
+    int64_t b = 1, j = 0;
+    while (j < num_buckets) {
+      b = j;
+      key = key * 2862933555777941757ULL + 1;
+      j = (b + 1) * (double(1LL << 31) / double((key >> 33) + 1));
+    }
+    return b;
   }
 
   Memento<MementoMap> m_memento;
