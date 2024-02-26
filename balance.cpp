1c1ba6642e8c4a707dd4576452f13112ad221fdb
robertovicario <rvicariodev@gmail.com>
2024-02-26 13:48:47 +0100

    testing DxHash

--- balance.cpp
+++ balance.cpp
@@ -217,11 +217,13 @@ int main(int argc, char *argv[]) {
                                num_removals, num_keys);
   } else if (algorithm == "dx") {
     return bench<DxEngine>("DxEngine", filename, anchor_set, working_set, num_removals, num_keys);
+  /*
   } else if (algorithm == "ring") {
     return bench<RingEngine>("RingEngine", filename, anchor_set, working_set, num_removals, num_keys);
   } else if (algorithm == "maglev") {
     return bench<MaglevEngine>("MaglevEngine", filename, anchor_set, working_set, num_removals, num_keys);
   } else {
+  */
     fmt::println("Unknown algorithm {}", algorithm);
     return 2;
   }
