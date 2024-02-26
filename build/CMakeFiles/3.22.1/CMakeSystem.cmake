801f5cc128a63529ae137e242889666e498c691d
robertovicario <rvicariodev@gmail.com>
2024-02-26 14:24:16 +0100

    update DxHash

--- build/CMakeFiles/3.22.1/CMakeSystem.cmake
+++ build/CMakeFiles/3.22.1/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Linux")
 set(CMAKE_HOST_SYSTEM_VERSION "6.5.0-17-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-
+include("/home/robertovicario/Scrivania/cpp-consistent-hashing-algorithms/vcpkg/scripts/buildsystems/vcpkg.cmake")
 
 set(CMAKE_SYSTEM "Linux-6.5.0-17-generic")
 set(CMAKE_SYSTEM_NAME "Linux")
