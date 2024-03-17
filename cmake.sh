b7b6f495928329154ef7a2c891579d53e583e3b7
Roberto Vicario <rvicariodev@gmail.com>
2024-03-17 01:11:01 +0100

    updates

---
+++ cmake.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Ensure script has executable permissions:
+# chmod +x setup.sh
+
+if [ -d "build" ]; then
+    rm -r build
+fi
+
+cmake -B build -S . -GNinja -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
--- repo.sh
+++ repo.sh
@@ -3,9 +3,6 @@
 # Ensure script has executable permissions:
 # chmod +x setup.sh
 
-# Update submodules
-# Change directory to vcpkg
-# Bootstrap vcpkg using system binaries
 git submodule update --init
 cd vcpkg || exit
 ./bootstrap-vcpkg.sh -useSystemBinaries
