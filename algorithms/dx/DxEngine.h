931b3fb083230f2b908798693f9850ec2c033324
Roberto Vicario <rvicariodev@gmail.com>
2024-03-22 14:38:47 +0100

    updates

--- algorithms/dx/DxEngine.h
+++ algorithms/dx/DxEngine.h
@@ -7,20 +7,20 @@
 #include "DxHash.hpp"
 
 class DxEngine {
-private:
-    DxHash dx;
+    private:
+        DxHash dx;
 
-public:
-    DxEngine(uint32_t anchor_set, uint32_t working_set)
-            : dx{anchor_set, working_set}
-            {}
+    public:
+        DxEngine(uint32_t anchor_set, uint32_t working_set)
+                : dx{anchor_set, working_set}
+                {}
 
-    uint32_t getBucketCRC32c(uint32_t key, uint32_t seed) { return dx.getNodeID(key, &seed); }
+        uint32_t getBucketCRC32c(uint32_t key, uint32_t seed) { return dx.getNodeID(key, &seed); }
 
-    uint32_t addBucket() { return dx.updateAddition(); }
+        uint32_t addBucket() { return dx.updateAddition(); }
 
-    uint32_t removeBucket(uint32_t bucket) {
-        dx.updateRemoval(bucket);
-        return bucket;
-    }
+        uint32_t removeBucket(uint32_t bucket) {
+            dx.updateRemoval(bucket);
+            return bucket;
+        }
 };
